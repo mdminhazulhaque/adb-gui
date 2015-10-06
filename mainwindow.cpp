da573dd7fe74b4b7edbcfe2b31ca80ed2c17dd36
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2015-10-06 23:53:45 +0600

    Maverick

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowTitle("adb-gui");
+    setWindowTitle("Android Debug Bridge - Frontend");
 }
 
 MainWindow::~MainWindow()
